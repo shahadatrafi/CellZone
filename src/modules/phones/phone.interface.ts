2f22c657e264885b4d329824efc67da45bb82911
unknown <mdshahadathossenrafi@gmail.com>
2024-08-08 22:08:37 +0600

    Created type for products

---
+++ src/modules/phones/phone.interface.ts
@@ -0,0 +1,20 @@
+export interface TProductVariant {
+    type: string;  
+    value: string; 
+  }
+  
+  export interface TProductInventory {
+    quantity: number;  
+    inStock: boolean;  
+  }
+  
+  export interface TProduct {
+    name: string;                  
+    description: string;           
+    price: number;                 
+    category: string;              
+    tags: string[];                
+    variants: TProductVariant[];    
+    inventory: TProductInventory;   
+  }
+  
